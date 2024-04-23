c1b1182f255e092e983eadda19e8a3183facbfb7
ahkarminmyat <dev.ahkarminmyat98@gmail.com>
2024-04-23 12:16:06 +0700

    create user

--- create-user.sql
+++ create-user.sql
@@ -3,3 +3,4 @@ CREATE USER 'sysadmin'@'192.168.1.%' IDENTIFIED BY 'Sys@123';
 SELECT User FROM mysql.user;
 SELECT User,Host FROM mysql.user;
 FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'neptunedb-admin'@'192.168.1.%';
